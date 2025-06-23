aef9fa2af1521d551bedc82f8393bf8c97e2c52c
ismailalakhras <ismail.malakhras@gmail.com>
2025-06-23 10:29:33 +0300

    create categories and subcategories tables

---
+++ app/Models/Subcategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subcategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'image',
+        'is_active',
+        'sort_order',
+        'meta_title',
+        'meta_description'
+    ];
+}
